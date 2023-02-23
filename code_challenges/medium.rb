6be08b90f4f0039f4ef6778ed6c4c60e44c846f5
William Zale <williamzale@gmail.com>
2023-02-23 17:04:59 -0500

    Add conditional statements to complete challenge

--- code_challenges/medium.rb
+++ code_challenges/medium.rb
@@ -9,6 +9,15 @@
 #- If the user either has a good record or is over 25 years old, they should pay full price
 #- If the user is not over 25 and has a bad driving record, they need to have someone else sign for the rental
 
-good_driving_record = true
-age = 24
+good_driving_record = false
+age = 5
+
+if good_driving_record == true && age > 25
+    puts "Congratulations, you get a discount on your car rental!"
+elsif
+    good_driving_record == true || age > 25
+    puts "Congratulations, you are eligible to rent a car."
+else
+    puts "Sorry, someone else will need to sign up to rent this car."
+end
 
